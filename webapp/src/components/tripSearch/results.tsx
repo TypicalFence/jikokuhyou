858b21de6923ef05521e33d5e454632c375156c9
Alex Fence <alexfence.code@gmail.com>
2019-07-19 21:00:09 +0200

    properly display results

--- webapp/src/components/tripSearch/results.tsx
+++ webapp/src/components/tripSearch/results.tsx
@@ -1,21 +1,38 @@
 import React from "react";
 import { TripDTO } from "sbb-webservice";
+import { StopDTO } from "sbb-webservice/src/protocol/api";
 
 interface ResultProps {
     trips: TripDTO[];
 }
 
-const Results = ({ trips }: ResultProps) => {
-    const content = trips.map((t: TripDTO) => (
-        <p>
-            <span>{t.products[0]}</span>
-            {" "}
-            {t.from.station.name}
-            {" "}
--
-            {" "}
-            {t.from.platform}
-        </p>
+function getTime(timeStamp: number|null): string {
+    if (timeStamp) {
+        const date = new Date(timeStamp);
+        const hours = date.getHours().toString().padStart(2, "0");
+        const minutes = date.getMinutes().toString().padStart(2, "0");
+
+        return `${hours}:${minutes}`;
+    }
+
+    return "";
+}
+
+const Results = ({ trips }: ResultProps): JSX.Element => {
+    const renderPlatform = (stop: StopDTO): string => `${stop.station.name} - Gl. ${stop.platform}`;
+
+    const content = trips.map((t: TripDTO): JSX.Element => (
+        <div className="columns">
+            <div className="column is-2">
+                <span className="icon">
+                    <i className="fas fa-train" />
+                </span>
+                {t.products[0]}
+            </div>
+            <div className="column is-4">{`${getTime(t.from.departure)} -> ${getTime(t.to.arrival)}`}</div>
+            <div className="column is-4">{`${renderPlatform(t.from)} -> ${renderPlatform(t.to)}`}</div>
+            <div className="column is-2">{t.duration}</div>
+        </div>
     ));
 
     return (
